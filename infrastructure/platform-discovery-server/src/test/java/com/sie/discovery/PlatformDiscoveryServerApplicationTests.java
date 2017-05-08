a631548b3c968d619ca9e6619a16451f41285a8d
Raya <2812849244@qq.com>
2017-05-08 11:49:34 +0800

    config server

---
+++ infrastructure/platform-discovery-server/src/test/java/com/sie/discovery/PlatformDiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sie.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PlatformDiscoveryServerApplication.class})
+public class PlatformDiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
