b90d8953ca7d6e2716f0311aa0677933b1f1c0fd
Raya <2812849244@qq.com>
2017-05-08 11:48:52 +0800

    config server

---
+++ infrastructure/platform-config-server/src/test/java/com/travelersun/conf/PlatformConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.travelersun.conf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PlatformConfigServerApplication.class})
+public class PlatformConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
