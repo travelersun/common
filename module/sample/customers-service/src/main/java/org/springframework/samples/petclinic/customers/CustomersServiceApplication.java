032caf3eb51af5818ecb7bb731ec83fbe9b58810
Raya <2812849244@qq.com>
2017-05-08 11:57:19 +0800

    sample customers service

---
+++ module/sample/customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author Maciej Szarlinski
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CustomersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersServiceApplication.class, args);
+	}
+}
