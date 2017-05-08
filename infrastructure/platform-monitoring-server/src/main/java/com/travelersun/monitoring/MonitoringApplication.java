83a7f88d43ddc0e0316b096c7e0bba7b7f04e9bc
Raya <2812849244@qq.com>
2017-05-08 11:51:36 +0800

    monitoring server

---
+++ infrastructure/platform-monitoring-server/src/main/java/com/travelersun/monitoring/MonitoringApplication.java
@@ -0,0 +1,19 @@
+package com.travelersun.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableTurbineStream
+@EnableHystrixDashboard
+//@EnableTurbine
+public class MonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+	}
+}
